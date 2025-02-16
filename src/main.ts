7d3ea2a9f6204ceeaeb18451de8bd616824ac2d3
Williams Hidrovo <40920477+Williams-Hidrovo@users.noreply.github.com>
2025-02-16 00:53:53 -0500

    configuracion dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(RouterModule.forRoot(routes)), provideAnimationsAsync()],
+});
